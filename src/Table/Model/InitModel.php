509ec3941de3af9773c9064a1752c6c86ee028ca
OuZhou <2250047157@qq.com>
2020-04-19 23:06:40 +0800

    修复insert语句限制

--- src/Table/Model/InitModel.php
+++ src/Table/Model/InitModel.php
@@ -11,49 +11,4 @@ class InitModel extends Model
     protected $dateFormat = 'Y-m-d H:i:s';// 时间格式
     protected $dates      = ['deleted_at', 'created_at', 'updated_at']; // 时间转换
     protected $hidden = ['password'];
-
-    /**
-     * 重写模型insert方法 自动注入时间,弥补了Laravel Model::insert() 不自动加入时间的问题。
-     * @param array $data
-     * @return bool
-     */
-    public function insert(array $data)
-    {
-        if ($this->timestamps == true) {
-            if (count($data, true) === count($data)) {
-                foreach ($data as &$item) {
-                    $item = self::mergeDataAt($item);
-                }
-                unset($item);
-            } else {
-                $data = self::mergeDataAt($data);
-            }
-        }
-
-        return static::insert($data);
-    }
-
-    /**
-     * @param array $data
-     * 重写 Model::insertGetId() 方法 添加时间
-     * @param null  $args
-     * @return int|mixed
-     */
-    public function insertGetId(array $data, $args = null)
-    {
-        if ($this->timestamps == true) {
-            $data = self::mergeDataAt($data);
-        }
-        return static::insertGetId($data, $args);
-    }
-
-    public static function mergeDataAt(array $data)
-    {
-
-        $time = now()->toDateString();
-        $data = array_merge($data, [
-            'created_at' => $time,
-        ]);
-        return $data;
-    }
 }
